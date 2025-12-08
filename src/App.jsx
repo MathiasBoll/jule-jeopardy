004b8ce62070712a4592c6a75ad1dc6643700e5f
torx <tor.vesterled@gmail.com>
2025-12-08 11:31:17 +0100

    Add Home page as landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import GamesDashboard from "./pages/CMS/GamesDashboard";
 import Kategory from "./pages/Kategory/Kategory";
 import GamesDashBoard from "./components/GamesDashBoard/GamesDashBoard";
+import Home from "./pages/Home/Home";
 import "./styles/global.css";
 import "./App.css";
 
@@ -14,6 +15,7 @@ import {
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/games-dashboard" element={<GamesDashboard />} />
       <Route path="/game-play" element={<GamesDashBoard />} />
       <Route path="/kategory" element={<Kategory />} />
