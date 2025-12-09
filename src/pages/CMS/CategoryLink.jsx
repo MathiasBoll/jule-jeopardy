59127fed0871f0ef1bc989e6b34e524fef20c5b2
Oleg Troian <115949684+sleiterr@users.noreply.github.com>
2025-12-09 12:59:51 +0100

    update button (#43)

--- src/pages/CMS/CategoryLink.jsx
+++ src/pages/CMS/CategoryLink.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import s from "./DashBoard.module.css";
 
-export const CategoryLink = ({ gameId, categoryId, label }) => {
+export const CategoryLink = ({ to, label, children, className }) => {
   return (
-    <Link className={s.link} to={`/kategory/${gameId}/${categoryId}`}>
-      {label}
+    <Link className={`${s.link} ${className}`} to={to}>
+      {label || children}
     </Link>
   );
 };
