31e4f565f8b36a11f44244fd0616acb24ac355c4
Mathias Boll <math50v7@mcdmstudent.dk>
2025-12-09 22:38:55 +0100

    Feature/mathias (#46)
    * Tilføjet mapper så strukturen er på plads
    
    * css til JeopardyQuestion samt kommentarer

--- src/pages/JeopardyQuestion/JeopardyQuestion.jsx
+++ src/pages/JeopardyQuestion/JeopardyQuestion.jsx
@@ -1,16 +1,26 @@
+// src/pages/JeopardyQuestion/JeopardyQuestion.jsx
+// Side-wrapper for admin-spørgsmålsformularen
+// - Viser DashNav i venstre side
+// - Centrerer selve formular-panelet i resten af viewporten
+
 import React from "react";
 import DashNav from "../DashNav/DashNav";
-import JeopardyQuestionForm from "../JeopardyQuestion/JeopardyQuestionForm";
+import JeopardyQuestionForm from "./JeopardyQuestionForm";
+
+// ✅ CSS’en ligger i samme mappe som denne fil
+import "./JeopardyQuestion.css";
 
 const JeopardyQuestion = () => {
   return (
-    <section className={`sectionAdmin`}>
+    <section className="sectionAdmin">
+      {/* Venstre admin-navigation */}
       <DashNav />
-      <div className={`admin-container`}>
-        <div className="">
-          <JeopardyQuestionForm />
-        </div>
-      </div>
+
+      {/* Højre område hvor selve panelet ligger */}
+      <main className="jq-admin-main">
+        {/* Formularen står for indholdet af panelet */}
+        <JeopardyQuestionForm />
+      </main>
     </section>
   );
 };
