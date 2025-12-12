0be07f6712a9d645953ba9e49591916f583a6a0f
Mathias Boll <math50v7@mcdmstudent.dk>
2025-12-12 09:39:20 +0100

    kommentarer tilføjet

--- src/pages/CMS/CategoryTableRow.jsx
+++ src/pages/CMS/CategoryTableRow.jsx
@@ -1,7 +1,26 @@
+// src/components/dashboard/CategoryTableRow.jsx
+// Genbrugelig tabel-celle-komponent til dashboard-tabeller
+// Wrapper omkring <td> der håndterer alignment og fælles styling
+// Bruges fx i spil-, kategori- og spørgsmålsoversigter
+
 import React from "react";
 import clsx from "clsx";
 import s from "./DashBoard.module.css";
 
+// CategoryTableRow
+// Props:
+// - align: tekstjustering i cellen ("left", "center", "right")
+//          matcher CSS-klasser i DashBoard.module.css
+// - children: indholdet af cellen (tekst, knapper, links osv.)
 export const CategoryTableRow = ({ align = "left", children }) => {
-  return <td className={clsx(s.cell, s[align])}>{children}</td>;
+  return (
+    <td
+      // clsx bruges til sikkert at kombinere flere CSS-klasser
+      // s.cell = basis-styling for alle tabelceller
+      // s[align] = dynamisk alignment (fx s.left, s.center, s.right)
+      className={clsx(s.cell, s[align])}
+    >
+      {children}
+    </td>
+  );
 };
